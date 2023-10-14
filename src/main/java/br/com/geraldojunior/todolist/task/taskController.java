88e3c874fad212043d3f62a05ed6420851dd7c14
Geraldo Junior <junior4231@hotmail.com>
2023-10-14 08:50:24 -0300

    Adicionando Opção de Delete na Tarefa

--- src/main/java/br/com/geraldojunior/todolist/task/taskController.java
+++ src/main/java/br/com/geraldojunior/todolist/task/taskController.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,9 +74,30 @@ public class taskController {
 
         Utils.copyNonNullProperties(taskModel, task);
         var taskUpdated = this.taskRepository.save(task);
-        return ResponseEntity.ok().body(taskUpdated);
-
-    
-        
+        return ResponseEntity.ok().body(taskUpdated);     
 }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Tarefa não encontrada");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Essa tarefa não pertence a você, Impossivel deletar");
+        }
+
+        this.taskRepository.delete(task);
+
+        return ResponseEntity.ok().body("Tarefa deletada com sucesso");
+
+    }
+
 }
